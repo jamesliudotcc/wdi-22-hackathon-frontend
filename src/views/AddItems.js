d00a0bb5b9a60a5c37b3f038a7848df36220a530
chidrome <chidrome@gmail.com>
2019-01-31 16:18:18 -0800

    added items, add items and store components

---
+++ src/views/AddItems.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export default class AddItems extends Component {
+    render() {
+        return (
+        <div>
+            <h1>Create a form to add items</h1>
+            <form>
+                <div className="form-control">
+                    <label name="itemName">Item Name</label>
+                    <input type="text" name="itemName" onChange={this.storeInput} value={this.state.itemName} />
+                </div>
+                <div className="form-control">
+                    <label name="user">For:</label>
+                    <input type="text" name="user" onChange={this.storeInput} value={this.state.user} />
+                </div>
+                <div></div>
+                <div className="form-control">
+                    <input type="submit" value="Add Item" />
+                </div>
+            
+            </form>
+        </div>
+        )
+    }
+}
---
+++ src/views/Items.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Items extends Component {
+    
+    render() {
+        return (
+        <div>
+            <h1>This is where we can show the Items</h1>
+        </div>
+        )
+    }
+}
